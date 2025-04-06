b268c9e19a974be1db389157abe2658fb2eb1bd3
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-04-06 10:11:09 +0200

    release 4.4

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Godot-Save-Template
-a template for save files for Godot Engine
+
+a template for save files for Godot Engine 4.4!
+
+Download the Repository and paste **only** the `save_template` folder into your Godot Project, then drag the `save.gd` script to the root of the `start` scene. In the `script.gd` are more instructions about saving the variables and loading them.
+
+*Feel free to fork this Repository and create a Pull Request!*
+
+see [LICENSE](LICENSE)
--- save_template/save.gd
+++ save_template/save.gd
@@ -2,9 +2,12 @@
 
 extends Node2D
 
+# saving the save folder path in a variable
 var save_path = "user://godot_save_template.save"
 
+# variables
 var saved_text = 0
+var saved_number = 0
 
 func _ready():
 	$"RichTextLabel".text = str(saved_text)
@@ -20,6 +23,7 @@ func _on_load_pressed() -> void:
 	load_data()
 
 func _on_folder_pressed() -> void:
+	# to open the save folder path
 	var absolute_path = ProjectSettings.globalize_path(save_path)
 	OS.shell_open(absolute_path.get_base_dir())
 
@@ -34,16 +38,31 @@ func _on_add_pressed():
 	$"RichTextLabel".text = str(saved_text)
 
 func save():
+	# save file access in a variable
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
+
+	# the variables we want to save 
 	file.store_var(saved_text)
+
+	# if we want to save another variables with the name: saved_number
+	file.store_var(saved_number)
 
 	print("Data saved")
 
 func load_data():
+	# check if there is a save file
 	if FileAccess.file_exists(save_path):
+
+		# save the file access in variable
 		var file = FileAccess.open(save_path, FileAccess.READ)
+
+		# load the variable from the save file
 		saved_text = file.get_var(saved_text)
 
+		# e.g. for another variable
+		saved_number = file.get_var(saved_number)
+
+		# just update the text label
 		$"RichTextLabel".text = str(saved_text)
 
 		print("Data Loaded")
